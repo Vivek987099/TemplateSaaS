7de7c14ce60209df13d46bf34fceaf32609d9f4e
Vivek Arya <viveksinghfzd8868965223@gmail.com>
2025-08-03 10:35:57 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='font-moonDance text-6xl font-bold text-[#090e34] ml-20  mt-25'>Complete Tailwind CSS <br />Template for SaaS Website</h1>
+    </div>
+  )
+}
+
+export default App
